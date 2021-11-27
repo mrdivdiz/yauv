604c57fa72eafece14e98773ac8a622c9917100b
mrdivdiz <divdizcraft@gmail.com>
2021-11-27 09:54:32 +0400

    v0.9.1
    latest update

---
+++ Assets/DoFD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class DoFD : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(InputManager.GetButton("Fire2")){
+			transform.gameObject.GetComponent<DepthOfFieldDeprecated>().enabled = true;
+		}else{
+			transform.gameObject.GetComponent<DepthOfFieldDeprecated>().enabled = false;
+		}
+	}
+}
